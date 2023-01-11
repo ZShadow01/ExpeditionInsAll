ae6fbc02bce08f56a243a7e8aab796cd5cd091fe
ZShadow01 <zshadow.sniper01@gmail.com>
2023-01-11 10:59:29 +0100

    Model loaded

--- scripts/model-loading.js
+++ scripts/model-loading.js
@@ -1,7 +1,10 @@
 AFRAME.registerComponent('model-loading', {
     init: function() {
-        this.el.addEventListener('model-loaded', (e) => {
-            this.el.classList.remove('loading');
+        this.el.parentElement.addEventListener('markerFound', (e) => {
+            this.el.classList.add('model-loaded');
+            this.el.addEventListener('model-loaded', (e) => {
+                this.el.classList.remove('loading');
+            });
         });
     }
 });
