e63fd8481aa4e018ab5014f202f32fac74de63cd
ZShadow01 <zshadow.sniper01@gmail.com>
2023-01-11 11:14:32 +0100

    Load animations

--- scripts/animation-load.js
+++ scripts/animation-load.js
@@ -1,5 +1,24 @@
 AFRAME.registerComponent('animation-load', {
     init: function() {
         this.loadAnimation = this.loadAnimation.bind(this);
+        this.loaded = false;
+        this.el.addEventListener('model-loaded', () => {
+            this.loadAnimation();
+        });
+    },
+    loadAnimation: function() {
+        if ('gltf-model' in this.el.components) {
+            const gltfModel = this.el.components['gltf-model'].model;
+            if (gltfModel.animations) {
+                const clips = gltfModel.animations[0];
+            
+                this.mixer = new THREE.AnimationMixer(gltfModel);
+                this.mixer.clipAction(clips).play();
+            }
+        }
+    },
+    tick: function (time, delta) {
+        if (!this.mixer) return;
+        this.mixer.update(delta / 1000);
     }
 });
--- scripts/model-loading.js
+++
@@ -1,7 +0,0 @@
-AFRAME.registerComponent('model-loading', {
-    init: function() {
-        this.el.addEventListener('model-loaded', (e) => {
-            alert('Loaded');
-        });
-    }
-});
