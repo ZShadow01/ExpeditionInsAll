5bd97b0ede3c12770891ead53050da146f9189a3
ZShadow01 <zshadow.sniper01@gmail.com>
2023-01-11 11:07:32 +0100

    Fixed typo

--- scripts/model-loading.js
+++ scripts/model-loading.js
@@ -1,11 +1,7 @@
 AFRAME.registerComponent('model-loading', {
     init: function() {
-        let element = this.el;
-        console.log(element);
-        element.parentElement.addEventListener('markerFound', (e) => {
-            element.addEventListener('model-loaded', (e) => {
-                element.classList.remove('loading');
-            });
+        this.el.addEventListener('model-loaded', (e) => {
+            alert('Loaded');
         });
     }
 });
